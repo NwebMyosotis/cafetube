9cbb33c994dd5943d737ee5470a6365dc4f2437d
NwebMyosotis <dynamickid@naver.com>
2023-02-16 14:52:14 +0900

    parameter url

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { getEdit, getUpload, getWatch } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", getWatch);
-videoRouter.get("/watch/edit", getEdit);
 videoRouter.get("/upload", getUpload);
+videoRouter.get("/:id([0-9a-f]{24})", getWatch);
+videoRouter.get("/:id([0-9a-f]{24})/edit", getEdit);
 
 export default videoRouter;
